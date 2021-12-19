89e065acd612428cdd2c4455694a8d199d7eac78
Arslan A <arslan@hillitilsimat.com>
2021-12-19 12:00:41 +0500

    refactor(footer.js): Fixed a typo in a color name

--- components/footer.js
+++ components/footer.js
@@ -2,12 +2,12 @@ import Link from 'next/link'
 
 const Footer = () => {
   return (
-    <div className="px-60 bg-vary_dark_grey">
+    <div className="px-60 bg-very_dark_grey">
       <div className="flex items-center justify-between min-h-min px-14 py-3">
-        <div className="text-vary_light_grey text-xs">
+        <div className="text-very_light_grey text-xs">
           © 2021/All rights reserved
         </div>
-        <div className="text-vary_light_grey flex items-center justify-center text-xs">
+        <div className="text-very_light_grey flex items-center justify-center text-xs">
           <div className=" px-2">developed by</div>
           <Link href="https://hillitilsimat.com">
             <a className="underline text-strong_blue hover:text-sm">
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,8 @@ module.exports = {
   ],
   theme: {
     colors: {
-      vary_dark_grey: '#353535',
-      vary_light_grey: '#E2E2E2',
+      very_dark_grey: '#353535',
+      very_light_grey: '#E2E2E2',
       strong_blue: '#1F97D8',
       with: '#FFFFFF',
       very_light_blue: '#7E9BFF',
