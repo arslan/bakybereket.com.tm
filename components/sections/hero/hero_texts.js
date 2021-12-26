d0114f780b88fb1ea60d7d0479706e216a6a339e
Arslan A <arslan@hillitilsimat.com>
2021-12-26 17:21:59 +0500

    fix(line.js,-hero_text.js): Fixed decorative line movement when switching languages

--- components/sections/hero/hero_texts.js
+++ components/sections/hero/hero_texts.js
@@ -1,30 +1,47 @@
-import { useTranslation } from "next-i18next";
+import { useTranslation } from 'next-i18next'
 
 function HeroTexts() {
-  const { t } = useTranslation("common");
+  const { t } = useTranslation('common')
 
   return (
     <div className="flex hero-texts mb-14 mt-4 gap-14 mr-12">
       <div>
-        <h2 className="w-72">{t('hero.hero_texts.first.heading')}</h2>
-        <p>
-          {t('hero.hero_texts.first.text')}
-        </p>
+        <h2 className="relative w-[17rem]">
+          {t('hero.hero_texts.first.heading')}
+          <div className="line absolute w-0.5 h-[2.5rem] -top-[1.5rem] bg-light-blue -right-8">
+            <div className="circle -left-[0.45rem] top-[1.8rem]">
+              <div className="inner-circle"></div>
+            </div>
+          </div>
+        </h2>
+        <p>{t('hero.hero_texts.first.text')}</p>
       </div>
       <div>
-        <h2 className="w-64">{t('hero.hero_texts.second.heading')}</h2>
-        <p>
-          {t('hero.hero_texts.second.text')}
-        </p>
+        <h2 className="relative w-fit">
+          {t('hero.hero_texts.second.heading')}
+          <div className="line absolute w-0.5 h-[2.5rem] -top-[1.5rem] bg-light-blue -right-8">
+            <div className="circle -left-[0.45rem] top-[1.8rem]">
+              <div className="inner-circle"></div>
+            </div>
+          </div>
+        </h2>
+        <p>{t('hero.hero_texts.second.text')}</p>
       </div>
       <div>
-        <h2>{t('hero.hero_texts.third.heading')}</h2>
-        <p>
-          {t('hero.hero_texts.third.text')}
-        </p>
+        <h2 className="relative w-fit">
+          {t('hero.hero_texts.third.heading')}
+          <div className="line absolute h-0.5 w-[36.8rem] bg-light-blue -right-8 -top-[1.52rem]"></div>
+          <div className="line absolute w-0.5 h-[2.5rem] -top-[1.5rem] bg-light-blue -right-8">
+            <div className="circle -left-[0.45rem] top-[1.8rem]">
+              <div className="inner-circle"></div>
+            </div>
+          </div>
+        </h2>
+
+        <p>{t('hero.hero_texts.third.text')}</p>
       </div>
     </div>
-  );
+  )
 }
 
-export default HeroTexts;
+export default HeroTexts
